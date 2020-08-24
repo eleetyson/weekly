07c5c2e4e2861abe0e3af82e980b68e3b049a160
Ethan <eleetyson@gmail.com>
2020-08-24 11:08:46 -0400

    add stylesheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,29 @@
 class ApplicationController < Sinatra::Base
 
+# set where the controllers should look for views
+# enable sessions
   configure do
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "pw_security"
   end
 
-  
+  # using the Sinatra Flash gem to display custom error messages
+  register Sinatra::Flash
+
+  get '/' do
+    redirect '/login'
+  end
+
+  helpers do
+    def current_user # helper method that returns the current user instance
+      User.find_by_id(session[:user_id])
+    end
+
+    def logged_in? # helper method that returns whether the current user is logged in
+      !!session[:user_id]
+    end
+  end
+
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+    puts params
+  end
+
+end
